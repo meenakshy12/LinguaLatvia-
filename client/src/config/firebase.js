2a5751344fcfa1380b7f9ec0f821263d1a04c987
apsdeveloper461 <mehboobalam786461@gmail.com>
2025-04-10 01:14:57 +0500

    remove mearsemenet of firebase

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -13,8 +13,7 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_FB_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FB_APP_ID,
-  measurementId: import.meta.env.VITE_FB_MEASUREMENT_ID
+  appId: import.meta.env.VITE_FB_APP_ID
 };
 
 
