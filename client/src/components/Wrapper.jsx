2ae658107e39c36c787d523d495dc1945f360aa8
apsdeveloper461 <mehboobalam786461@gmail.com>
2025-04-28 06:28:42 +0500

    check on deployment bg color height

--- client/src/components/Wrapper.jsx
+++ client/src/components/Wrapper.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import SideBar from './Sidebar/SideBar'
 import Navbar from './Navbar'
 
 const Wrapper = ({children}) => {
   return (
-    <div className="flex flex-col min-h-screen relative h-full bg-gradient-to-b from-blue-100 to-blue-50 ">
+    <div className="flex flex-col min-h-[100dvh] relative h-full bg-gradient-to-b from-blue-100 to-blue-50 ">
                 <Navbar />
     {/* <div className="flex gap-3 relative h-full w-full z-0 "> */}
         {/* <SideBar/> */}
