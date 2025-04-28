1aace1432f2dbeb636d363ad3d9688aa573ba807
apsdeveloper461 <mehboobalam786461@gmail.com>
2025-04-28 06:41:00 +0500

    set nine dots colors

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -48,50 +48,61 @@ const Navbar = () => {
     }
 
     return (
-        <nav className={`z-100  navbar w-screen shadow-sm flex justify-between h-16 pb-3 pt-5 px-10`} >
-            <div className="navbar-left">
-                <span className="navbar-logo text-xl font-bold text-[#1B4A7E] cursor-pointer" onClick={()=>handelNavigation("/")}>LinguaLatvia</span>
-            </div>
-            <div className="navbar-right relative" ref={dropdownRef}>
-                <div
-                    className="profile-icon cursor-pointer overflow-hidden "
-                  
-                    onClick={toggleDropdown}
-                >
-                    {/* <img src={avator} alt="Profile" className="w-full h-full object-cover" /> */}
-                    <TbGridDots className='text-2xl' />
-                </div>
-                {dropdownOpen && (
-                    <div className="dropdown-menu absolute -right-3 mt-2 w-62 md:w-72 flex flex-col justify-center pt-3 bg-white text-black rounded-2xl shadow-lg">
-                        
-                    <img src={avator} alt="Profile" className="w-10 h-10 rounded-full border m-auto border-slate-400 object-cover" />
+      <nav
+        className={`z-100  navbar w-screen shadow-sm flex justify-between h-16 pb-3 pt-5 px-10`}
+      >
+        <div className="navbar-left">
+          <span
+            className="navbar-logo text-xl font-bold text-[#1B4A7E] cursor-pointer"
+            onClick={() => handelNavigation("/")}
+          >
+            LinguaLatvia
+          </span>
+        </div>
+        <div className="navbar-right relative" ref={dropdownRef}>
+          <div
+            className="profile-icon cursor-pointer overflow-hidden "
+            onClick={toggleDropdown}
+          >
+            {/* <img src={avator} alt="Profile" className="w-full h-full object-cover" /> */}
+            <TbGridDots className="text-2xl text-[#1B4A7E]" />
+          </div>
+          {dropdownOpen && (
+            <div className="dropdown-menu absolute -right-3 mt-2 w-62 md:w-72 flex flex-col justify-center pt-3 bg-white text-black rounded-2xl shadow-lg">
+              <img
+                src={avator}
+                alt="Profile"
+                className="w-10 h-10 rounded-full border m-auto border-slate-400 object-cover"
+              />
 
-                        <div className="dropdown-item px-4 py-2 cursor-pointer truncate">{auth.currentUser?.email}</div>
-                        <div
-                            className="dropdown-item px-4 py-2 cursor-pointer flex items-center gap-2 group hover:font-semibold" 
-                            onClick={()=>handelNavigation("/")}
-                        >
-                            <RiHome3Line     className="group-hover:font-semibold" />
-                            Home
-                        </div>
-                        <div
-                            className="dropdown-item px-4 py-2 cursor-pointer flex items-center gap-2 group hover:font-semibold" 
-                            onClick={()=>handelNavigation("/Chatbot")}
-                        >
-                            <RiRobot2Line    className="group-hover:font-semibold" />
-                            Chatbot
-                        </div>
-                         <div
-                            className="dropdown-item px-4 py-2 cursor-pointer flex items-center gap-2 group hover:font-semibold" 
-                            onClick={handleLogout}
-                        >
-                            <CiLogout className="group-hover:font-semibold" />
-                            Logout
-                        </div>
-                    </div>
-                )}
+              <div className="dropdown-item px-4 py-2 cursor-pointer truncate">
+                {auth.currentUser?.email}
+              </div>
+              <div
+                className="dropdown-item px-4 py-2 cursor-pointer flex items-center gap-2 group hover:font-semibold"
+                onClick={() => handelNavigation("/")}
+              >
+                <RiHome3Line className="group-hover:font-semibold" />
+                Home
+              </div>
+              <div
+                className="dropdown-item px-4 py-2 cursor-pointer flex items-center gap-2 group hover:font-semibold"
+                onClick={() => handelNavigation("/Chatbot")}
+              >
+                <RiRobot2Line className="group-hover:font-semibold" />
+                Chatbot
+              </div>
+              <div
+                className="dropdown-item px-4 py-2 cursor-pointer flex items-center gap-2 group hover:font-semibold"
+                onClick={handleLogout}
+              >
+                <CiLogout className="group-hover:font-semibold" />
+                Logout
+              </div>
             </div>
-        </nav>
+          )}
+        </div>
+      </nav>
     );
 };
 
